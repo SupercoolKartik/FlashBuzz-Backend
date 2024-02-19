d79063d93c33237accec63d85447c9ae074d3020
Suraj <surajkartik71@gmail.com>
2024-02-20 01:19:29 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+const app = express();
+//import ip from "ip";
+//const serverIp = ip.address();
+const PORT = 5000;
+app.use(cors());
+
+// Handle requests for news data
+app.get("/", async (req, res) => {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiUrl = `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=${apiKey}`;
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    console.log(`Failed to fetch data: ${response.statusText}`);
+  }
+  const parsedData = await response.json();
+  //console.log(parsedData);
+  res.json(parsedData);
+});
+
+// Handle requests for manifest.json
+app.get("/manifest.json", (req, res) => {
+  // Customize the content of your manifest.json file
+  const manifest = {
+    name: "Your App Name",
+    short_name: "Short Name",
+    // Add other manifest properties as needed
+  };
+  res.json(manifest);
+});
+
+app.listen(PORT, () => {
+  // console.log(`App is listening at http://${serverIp}:${PORT}`);
+  console.log(`App is listening at http://localhost:${PORT}`);
+});
